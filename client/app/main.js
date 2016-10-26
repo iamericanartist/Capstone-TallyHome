a99ae1043443717bb11486fb64641d00b07d45c8
Eric Denton <eric@iamericanartist.com>
2016-10-26 14:16:12 -0500

    added angular-route, init routes, partials, ctrlrs, ng-view, hard coded data

--- client/app/main.js
+++ client/app/main.js
@@ -1,8 +1,24 @@
 "use strict"
 
 angular
-  .module("tallyHome", [])
-  .controller("main", function ($scope, $http) {          //add $http
+  .module("tallyHome", ["ngRoute"])                       //setup "tallyHome" app //inject ["ngRoute"] to make available to controllers
+
+  .config($routeProvider =>
+    $routeProvider
+///////////////////////////////////  ROUTES  ///////////////////////////////////
+      .when("/", {                                        //when at "/"
+        controller: "MainCtrl",                           //use "MainCtrl" controller (below)
+        templateUrl: "partials/main.html",                //and show "main.html" partial
+      })
+
+      .when("/homes", {
+        controller: "HomeCtrl",
+        templateUrl: "partials/homes.html",
+      })
+  )
+
+/////////////////////////////////  CONTROLLERS  /////////////////////////////////
+  .controller("MainCtrl", function ($scope, $http) {      //add $http
     $http
       .get("/api/title")
       .then(({ data: { title }}) =>                       //destructured from "data"
@@ -10,3 +26,10 @@ angular
       )
   })
 
+  .controller("HomeCtrl", function ($scope, $http) {
+    $http
+      .get("/api/homes")
+      .then(({ data: { homes }}) =>
+        $scope.homes = homes
+      )
+  })
